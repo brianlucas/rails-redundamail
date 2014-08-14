7f2a12e6cd8e3d63e1b260adf723b99771f3a98e
Brian Lucas <brian.lucas@gmail.com>
2014-08-14 12:47:54 -0700

    secret_token workaround

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : "top_secret_token"
-YourApp::Application.config.secret_key_base = secret
+Redundamail::Application.config.secret_key_base = secret
